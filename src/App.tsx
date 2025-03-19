b737f21e56c77f551d6ac43a67aba6d58b742c78
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-03-18 23:48:14 -0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,25 @@ import { Button } from "./components/ui/button";
 
 export function App(){
   return(
-    <div className="bg-black h-screen text-white">
-      <h1>Hello World</h1>
-      <p>
-        <span className="p-2 px-3 bg-violet-700">
+    <div className="bg-black h-screen text-white p-20">
+      <h1 className="my-10 p-1 text-3xl">Hello World !</h1>
+      <p className="">
+        <span className="p-2 px-3 bg-violet-700 mr-2">
           React 18+
         </span> 
         
-        <span className="p-2 px-3 bg-sky-700">
+        <span className="p-2 px-3 bg-sky-700 mr-2">
           TailwindCSS
         </span>
         
-        <span className="p-2 px-3 bg-gray-900 text-white">
+        <span className="p-2 border px-3 bg-gray-900 text-white">
         ShadCN-UI
         </span>
       </p>
       <br />
       <hr />
       <br />
-      <h2>ShadCN</h2>
-      <Button variant={"ghost"}>Entrar</Button>
+      <Button variant={"ghost"} className="border cursor-pointer">Entrar</Button>
     </div>
   )
 }
